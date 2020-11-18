05524b5ee91fb33a0fd560bd751e450bbd8f7afa
John Doe <johndoe@example.com>
2020-11-18 15:16:00 -0600

    i think i finished day 3

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,6 +7,6 @@ app_name = 'learning_logs'
 urlpatterns = [ #we are creating several urls
     path('',views.index, name='index'), #null because it is our home page, the name of the url is index
     path('topics',views.topics, name='topics'), #this is the topics url
-    path('topics/<int:topic_id>/',views.topics, name='topics'), #topics_id comes from the database
-]
+    path('topics/<int:topic_id>/',views.topic, name='topic'), #topics_id comes from the database
+    ]
 
